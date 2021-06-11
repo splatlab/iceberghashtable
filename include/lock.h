ddea816f687727eb1a2625131e631029455c0db3
prashant <Prashant Pandey>
2021-06-10 17:18:37 -0700

    separate lock c file.

--- include/lock.h
+++ include/lock.h
@@ -37,61 +37,16 @@ extern "C" {
     pc_t * pc_counter;
   } ReaderWriterLock;
 
-  void rw_lock_init(ReaderWriterLock *rwlock) {
-    rwlock->readers = 0;
-    rwlock->writer = 0;
-    rwlock->pc_counter = (pc_t *)malloc(sizeof(pc_t));
-    pc_init(rwlock->pc_counter, &rwlock->readers, 8, 8);
-  }
 
-  /**
-   * Try to acquire a lock and spin until the lock is available.
-   */
-  bool read_lock(ReaderWriterLock *rwlock, uint8_t flag, uint8_t thread_id) {
-    if (GET_WAIT_FOR_LOCK(flag) != WAIT_FOR_LOCK) {
-      if (rwlock->writer == 0) {
-        pc_add(rwlock->pc_counter, 1, thread_id);
-        return true;
-      }
-    } else {
-      while (rwlock->writer != 0);
-      pc_add(rwlock->pc_counter, 1, thread_id);
-      return true;
-    }
+void rw_lock_init(ReaderWriterLock *rwlock);
 
-    return false;
-  }
+bool read_lock(ReaderWriterLock *rwlock, uint8_t flag, uint8_t thread_id);
 
-  void read_unlock(ReaderWriterLock *rwlock, uint8_t thread_id) {
-    pc_add(rwlock->pc_counter, -1, thread_id);
-    return;
-  }
+void read_unlock(ReaderWriterLock *rwlock, uint8_t thread_id);
 
-  /**
-   * Try to acquire a write lock and spin until the lock is available.
-   * Then wait till reader count is 0.
-   */
-  bool write_lock(ReaderWriterLock *rwlock, uint8_t flag) {
-    // acquire write lock.
-    if (GET_WAIT_FOR_LOCK(flag) != WAIT_FOR_LOCK) {
-      if (__sync_lock_test_and_set(&rwlock->writer, 1))
-        return false;
-    } else {
-      while (__sync_lock_test_and_set(&rwlock->writer, 1))
-        while (rwlock->writer != 0);
-    }
-    // wait for readers to finish
-    do {
-      pc_sync(rwlock->pc_counter);
-    } while(rwlock->readers);
+bool write_lock(ReaderWriterLock *rwlock, uint8_t flag);
 
-    return true;
-  }
-
-  void write_unlock(ReaderWriterLock *rwlock) {
-    __sync_lock_release(&rwlock->writer);
-    return;
-  }
+void write_unlock(ReaderWriterLock *rwlock);
 
 #ifdef __cplusplus
 }
---
+++ src/lock.h
@@ -0,0 +1,72 @@
+/*
+ * ============================================================================
+ *
+ *         Author:  Prashant Pandey (), ppandey@cs.stonybrook.edu
+ *   Organization:  Stony Brook University
+ *
+ * ============================================================================
+ */
+
+#if !defined(_GNU_SOURCE)
+#define _GNU_SOURCE
+#endif
+
+#include "lock.h"
+
+void rw_lock_init(ReaderWriterLock *rwlock) {
+  rwlock->readers = 0;
+  rwlock->writer = 0;
+  rwlock->pc_counter = (pc_t *)malloc(sizeof(pc_t));
+  pc_init(rwlock->pc_counter, &rwlock->readers, 8, 8);
+}
+
+/**
+ * Try to acquire a lock and spin until the lock is available.
+ */
+bool read_lock(ReaderWriterLock *rwlock, uint8_t flag, uint8_t thread_id) {
+  if (GET_WAIT_FOR_LOCK(flag) != WAIT_FOR_LOCK) {
+    if (rwlock->writer == 0) {
+      pc_add(rwlock->pc_counter, 1, thread_id);
+      return true;
+    }
+  } else {
+    while (rwlock->writer != 0);
+    pc_add(rwlock->pc_counter, 1, thread_id);
+    return true;
+  }
+
+  return false;
+}
+
+void read_unlock(ReaderWriterLock *rwlock, uint8_t thread_id) {
+  pc_add(rwlock->pc_counter, -1, thread_id);
+  return;
+}
+
+/**
+ * Try to acquire a write lock and spin until the lock is available.
+ * Then wait till reader count is 0.
+ */
+bool write_lock(ReaderWriterLock *rwlock, uint8_t flag) {
+  // acquire write lock.
+  if (GET_WAIT_FOR_LOCK(flag) != WAIT_FOR_LOCK) {
+    if (__sync_lock_test_and_set(&rwlock->writer, 1))
+      return false;
+  } else {
+    while (__sync_lock_test_and_set(&rwlock->writer, 1))
+      while (rwlock->writer != 0);
+  }
+  // wait for readers to finish
+  do {
+    pc_sync(rwlock->pc_counter);
+  } while(rwlock->readers);
+
+  return true;
+}
+
+void write_unlock(ReaderWriterLock *rwlock) {
+  __sync_lock_release(&rwlock->writer);
+  return;
+}
+
+
