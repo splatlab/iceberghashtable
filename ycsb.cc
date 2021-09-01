4b9ad9fce8085ad186f417bab6cc52893ccdeca4
prashant <Prashant Pandey>
2021-08-31 18:19:42 -0700

    multi-resize

--- ycsb.cc
+++ ycsb.cc
@@ -227,11 +227,8 @@ void ycsb_load_run_randint(int index_type, int wl, int kt, int ap, int num_threa
                     } else if (ops[i] == OP_READ) {
                         uintptr_t *val;
                         auto ret = iceberg_get_value(tds[thread_id].ht, keys[i], &val, thread_id);
-                        if (!ret) {
-                            std::cout << "[ICEBERG] failed query: " << *val << " expected: " << keys[i] << std::endl;
-                        }
                         if (*val != keys[i]) {
-                            std::cout << "[ICEBERG] wrong key read: " << *val << " expected: " << keys[i] << std::endl;
+                            std::cout << "[ICEBERG] wrong key read: " << *val << " expected: " << keys[i] << " ret: " << ret << std::endl;
                             exit(1);
                         }
                     } else if (ops[i] == OP_SCAN) {
